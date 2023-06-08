116379fb05fe9585857068a2d5e11d1b8362a157
moodyzeromn <moodyzeromn@gmail.com>
2023-06-08 16:57:24 +0800

    s

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -2,9 +2,13 @@ import 'package:intl/intl.dart';
 
 class Utils {
   formatCurrency(value) {
+    var result;
+    if (value == 0) {
+      return result = '0.00';
+    }
     var formattedNumber =
         NumberFormat("#,###.00", "en_US").format(double.parse(value));
-    var result = formattedNumber.replaceAllMapped(
+    result = formattedNumber.replaceAllMapped(
         RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'), (Match m) => "${m[1]}'");
 
     return result;
